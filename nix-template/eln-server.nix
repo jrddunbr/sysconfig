28831a64665aafdef4480f3e28db132f66ace670
Jared Dunbar <jrddunbr@gmail.com>
2020-09-12 18:59:51 -0400

    Add 7z to minecraft.

--- nix-template/eln-server.nix
+++ nix-template/eln-server.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   networking.firewall.allowedTCPPorts = [ 25565 ];
-  environment.systemPackages = with pkgs; [ openjdk8 screen ];
+  environment.systemPackages = with pkgs; [ openjdk8 screen p7zip ];
   users.users.minecraft = {
     isNormalUser = true;
   };
