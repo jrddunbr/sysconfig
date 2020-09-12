bf7654b7a781a0b83358b83581704cacf845b6de
Jared Dunbar <jrddunbr@gmail.com>
2020-09-12 14:27:33 -0400

    Adds user to libvirtd group.

--- nix-template/vm-host.nix
+++ nix-template/vm-host.nix
@@ -2,4 +2,7 @@
 {
   # I dislike that it uses the British English spelling when everything else seems to be American English.
   virtualisation.libvirtd.enable = true;
+
+  # Add users to libvirtd so that they can do the thing
+  users.users.jared.extraGroups = [ "libvirtd" ];
 }
