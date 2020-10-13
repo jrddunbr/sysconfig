313efc40b15e5698a087209a7be115207e53b98b
Jared Dunbar <jrddunbr@gmail.com>
2020-10-12 20:11:21 -0400

    Add mattermost server

---
+++ vars/mm01.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+  imports =
+    [
+      /sysconfig/vars/hardware/eln01.nix
+      /sysconfig/nix-template/base-system.nix
+      /sysconfig/nix-template/users.nix
+    ];
+
+  networking = {
+    hostName = "mm01";
+    defaultGateway = { address = "10.0.0.1"; interface = "net0"; };
+    interfaces.net0.useDHCP = false;
+    interfaces.net0.ipv4.addresses =
+      [ { address = "10.0.0.31"; prefixLength = 24; } ];
+    firewall.allowedTCPPorts = [ 22 ];
+  };
+}
