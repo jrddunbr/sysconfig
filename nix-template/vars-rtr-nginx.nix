45ac53254a175ebb9850f394051a94dd5d4c7782
Jared Dunbar <jrddunbr@gmail.com>
2020-10-12 21:31:18 -0400

    Derp x2.

--- nix-template/vars-rtr-nginx.nix
+++ nix-template/vars-rtr-nginx.nix
@@ -7,14 +7,6 @@
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
 
-
-    services.nginx.enable = true;
-      services.nginx.virtualHosts.gitea.locations."/".proxyPass = "http://127.0.0.1:3000";
-      services.nginx.virtualHosts.gitea.locations."/".proxyWebsockets = true;
-      services.nginx.virtualHosts.gitea.serverName = "git.ja4.org";
-      services.nginx.virtualHosts.gitea.forceSSL = true;
-      services.nginx.virtualHosts.gitea.enableACME = true;
-
     virtualHosts = {
       chat = {
         locations."/" = {
