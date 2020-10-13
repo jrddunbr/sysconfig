eb962b34df936396bbb2d7f064f51fa4f9edc552
Jared Dunbar <jrddunbr@gmail.com>
2020-10-12 21:30:26 -0400

    Fix.

--- nix-template/vars-rtr-nginx.nix
+++ nix-template/vars-rtr-nginx.nix
@@ -7,17 +7,27 @@
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
 
+
+    services.nginx.enable = true;
+      services.nginx.virtualHosts.gitea.locations."/".proxyPass = "http://127.0.0.1:3000";
+      services.nginx.virtualHosts.gitea.locations."/".proxyWebsockets = true;
+      services.nginx.virtualHosts.gitea.serverName = "git.ja4.org";
+      services.nginx.virtualHosts.gitea.forceSSL = true;
+      services.nginx.virtualHosts.gitea.enableACME = true;
+
     virtualHosts = {
-      "chat.ja13.org" = {
+      chat = {
         locations."/" = {
           proxyPass = "http://10.0.0.31:8065";
           proxyWebsockets = true;
-          forceSSL = true;
-          enableACME = true;
         };
+        serverName = "chat.ja13.org";
+        forceSSL = true;
+        enableACME = true;
       };
     };
   };
 
+  security.acme.acceptTerms = true;
   security.acme.certs."chat.ja13.org".email = "chat@ja13.org";
 }
