e6ce57b8b9c37c4459434dce19733e8bbe0d4af1
Jared Dunbar <jrddunbr@gmail.com>
2020-10-24 19:28:59 -0400

    Add sqlite dep again

--- nix-template/vintage-story.nix
+++ nix-template/vintage-story.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   networking.firewall.allowedTCPPorts = [ 42420 ];
-  environment.systemPackages = with pkgs; [ mono sqlite ];
+  environment.systemPackages = with pkgs; [ mono sqlite.out ];
   users.users.vintagestory = {
     isNormalUser = true;
   };
